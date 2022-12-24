734fa7a12bf8c66d3bee062b1d989d4b2fa1e89d
ODEGOV ILYA 132 <114457146+At3K1@users.noreply.github.com>
2022-12-24 21:55:45 +0700

    Add files via upload

---
+++ команды питон.py
@@ -0,0 +1,40 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "dc44872c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "print\n",
+    "if\n",
+    "else\n",
+    "elif\n",
+    "range in\n",
+    "input\n",
+    "int\n",
+    "def\n",
+    "len\n",
+    "for\n",
+    "count\n",
+    "//\n",
+    "==\n",
+    "list\n",
+    "/\n",
+    "-\n",
+    "+\n",
+    "%\n",
+    "**\n",
+    "*"
+   ]
+  }
+ ],
+ "metadata": 
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   sp=[x for x in range(8)]
+ print (sp)
+   "language": "python",
+   "name": "python3"
+  }}
